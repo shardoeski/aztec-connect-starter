d26f10fb419668ec1c8a0dedef10f3c2de308e13
Tajobin <tajobin@protonmail.com>
2022-01-06 22:34:18 +0100

    Setting up test for yearn

---
+++ src/contracts/yearn_bridge.test.ts
@@ -0,0 +1,14 @@
+import { ethers } from 'hardhat';
+import { deployErc20 } from '../deploy/deploy_erc20';
+import { Contract, Signer } from 'ethers';
+import { DefiBridgeProxy, AztecAssetType } from './defi_bridge_proxy';
+
+describe('yearn bridge', function () {
+
+  beforeAll(async () => {
+
+  });
+
+  it('', async () => {
+
+});
